626fbe2f95c9e288a2353f288f856d41f8d75268
Oscar Castro <oscar.a.castro818@gmail.com>
2024-09-07 10:33:54 -0500

    remove code

--- app/api/servicefusion/token/refresh/route.js
+++ app/api/servicefusion/token/refresh/route.js
@@ -1,23 +1,24 @@
-import connectDB from "@/lib/connectDB";
-import Credentials from "@/models/credentials";
+// import connectDB from "@/lib/connectDB";
+// import Credentials from "@/models/credentials";
 
 export async function GET (req) {
-    await connectDB();
-    const credentials = await Credentials.findById("66dad17f465d12d0ab01513d");
-    const res = await fetch("https://api.servicefusion.com/oauth/access_token", {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "grant_type": "refresh_token",
-            "refresh_token": credentials.serviceFusionRefreshToken
-        }),
-    });
-    const data = await res.json();
-    credentials.serviceFusionAuthToken = data.access_token
-    credentials.serviceFusionRefreshToken = data.refresh_token
-    const updatedCredentials = await credentials.save();
-    return Response.json(updatedCredentials);
+    // await connectDB();
+    // const credentials = await Credentials.findById("66dad17f465d12d0ab01513d");
+    // const res = await fetch("https://api.servicefusion.com/oauth/access_token", {
+    //     method: "POST",
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     },
+    //     body: JSON.stringify({
+    //         "grant_type": "refresh_token",
+    //         "refresh_token": credentials.serviceFusionRefreshToken
+    //     }),
+    // });
+    // const data = await res.json();
+    // credentials.serviceFusionAuthToken = data.access_token
+    // credentials.serviceFusionRefreshToken = data.refresh_token
+    // const updatedCredentials = await credentials.save();
+    // return Response.json(updatedCredentials);
+    return new Response("hello")
 
 }
